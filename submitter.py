1c59dec5038637cc57a9d408b9dd8576c5448651
miketheologitis <miketh@outlook.com.gr>
2024-02-25 20:36:52 +0200

    f

--- submitter.py
+++ submitter.py
@@ -34,7 +34,7 @@ module load intelmpi/2018
 module load python/3.8.13
 module load tftorch/270-191
 
-echo node name, name of the current node: ${SLURMD_NODENAME}
+echo node name, name of the current node: $SLURMD_NODENAME
 echo job node list, list of all nodes allocated to the job: ${SLURM_JOB_NODELIST}
 echo job num nodes, number of nodes the job is using: ${SLURM_JOB_NUM_NODES}
 echo step node list: ${SLURM_STEP_NODELIST}
