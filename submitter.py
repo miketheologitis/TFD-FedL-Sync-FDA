d49ae2c07b71ab766596c3709cf2ebc6890c8d5c
miketheologitis <miketh@outlook.com.gr>
2024-02-25 20:37:18 +0200

    f

--- submitter.py
+++ submitter.py
@@ -35,9 +35,9 @@ module load python/3.8.13
 module load tftorch/270-191
 
 echo node name, name of the current node: $SLURMD_NODENAME
-echo job node list, list of all nodes allocated to the job: ${SLURM_JOB_NODELIST}
-echo job num nodes, number of nodes the job is using: ${SLURM_JOB_NUM_NODES}
-echo step node list: ${SLURM_STEP_NODELIST}
+echo job node list, list of all nodes allocated to the job: $SLURM_JOB_NODELIST
+echo job num nodes, number of nodes the job is using: $SLURM_JOB_NUM_NODES
+echo step node list: $SLURM_STEP_NODELIST
 echo "Running program"
 
 export TF_XLA_FLAGS="--tf_xla_enable_xla_devices"
